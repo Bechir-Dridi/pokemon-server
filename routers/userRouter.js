69ddf5627e3916985647ad4285de878019e49000
Bechir-Dridi <99827725+Bechir-Dridi@users.noreply.github.com>
2023-05-12 20:36:02 +0100

    try to fix logout cors pb

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -73,7 +73,7 @@ router.post("/login", async (req, res) => {
 
 //........ logout ........
 router.get("/logout", (req, res) => {
-    res.cookie("token", "", { httpOnly: true, secure: true, sameSite: "none", expires: new Date(0) })
+    res.cookie("token", "", { httpOnly: true, expires: new Date(0) })
         .json({ status: "you are logged out" });
 })
 
