d48c8f336c69abd5591c13e33c79c221c81d662e
Bechir-Dridi <99827725+Bechir-Dridi@users.noreply.github.com>
2023-05-12 19:27:57 +0100

    2nd fix cors pb

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.use("/uploads", express.static("uploads"));
 //avoid cors deployment problem, 
 //so we allow web pages from making requests to a different domain.
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin');
     res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
     // allow preflight requests
